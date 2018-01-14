7dab52f3cc5d30164b05c395ef10c3cb555dc530
Bruno <34379550+brund22@users.noreply.github.com>
2018-01-13 23:22:59 -0200

    Add files via upload

---
+++ MostraData/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Ol
+ * Date: 13/01/2018
+ * Time: 22:38
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace MostraData
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label Data;
+		private System.Windows.Forms.Label label1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.Data = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(29, 154);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(137, 35);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Clique";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(29, 112);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(137, 20);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(29, 49);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(137, 20);
+			this.textBox2.TabIndex = 2;
+			// 
+			// Data
+			// 
+			this.Data.Location = new System.Drawing.Point(29, 23);
+			this.Data.Name = "Data";
+			this.Data.Size = new System.Drawing.Size(88, 23);
+			this.Data.TabIndex = 3;
+			this.Data.Text = "Data:";
+			this.Data.Click += new System.EventHandler(this.DataClick);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(29, 82);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(77, 17);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Hora:";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(199, 219);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.Data);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "MostraData";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MostraData/MainForm.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Ol
+ * Date: 13/01/2018
+ * Time: 22:38
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MostraData
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+DateTime a = DateTime.Now;
+string data = DateTime.Now.ToString("dd:mm:yyyy");
+string hora = DateTime.Now.ToString("HH:mm:ss");
+DateTime dt = Convert.ToDateTime(a); 
+textBox1.Text = ""+hora ;
+textBox2.Text = ""+data ;
+        
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+
+
+		}
+		void DataClick(object sender, EventArgs e)
+		{
+	
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
